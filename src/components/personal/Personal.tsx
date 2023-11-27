75fbb1f11356ba675275ee4b488e8eec0bae6894
k_95.21 <kohta0421kochi@gmail.com>
2023-11-27 17:02:34 +0900

    作り直しpersonal

--- src/components/personal/Personal.tsx
+++ src/components/personal/Personal.tsx
@@ -1,20 +1,48 @@
-'use client'
 import React from 'react'
-import { useForm } from 'react-hook-form'
+import { usePersonal } from '@/hooks/usePersonal';
 
-// scss import
-import styles from './Personal.module.scss'
-
-// components import
-
-// zod
-import { ContactPersonalSchema, ContactPersonalType } from '@/schema/personal'
-import { zodResolver } from '@hookform/resolvers/zod'
-
-const Personal = () => {
+export const Personal = () => {
+    const { register, onSubmit, errors } = usePersonal();
     return (
-        <div className={styles.personal}>
-
+        <div>
+            <form onSubmit={onSubmit}>
+                <div>
+                    <label htmlFor="name">名前</label>
+                    <input
+                        id="name"
+                        type="text"
+                        {...register('name')}
+                    />
+                    {errors.name && <p>{errors.name.message}</p>}
+                </div>
+                <div>
+                    <label htmlFor="email">メールアドレス</label>
+                    <input
+                        id="email"
+                        type="text"
+                        {...register('email')}
+                    />
+                    {errors.email && <p>{errors.email.message}</p>}
+                </div>
+                <div>
+                    <label htmlFor="tel">電話番号</label>
+                    <input
+                        id="tel"
+                        type="text"
+                        {...register('telephone')}
+                    />
+                    {errors.telephone && <p>{errors.telephone.message}</p>}
+                </div>
+                <div>
+                    <label htmlFor="message">お問い合わせ内容</label>
+                    <textarea
+                        id="message"
+                        {...register('message')}
+                    />
+                    {errors.message && <p>{errors.message.message}</p>}
+                </div>
+                <button type="submit">送信</button>
+            </form>
         </div>
     )
 }
